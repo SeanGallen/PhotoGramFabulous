9c9a67cb8491dc4d8b49aa48394a5a7bee9aafa6
Sean <sjgallen@gmail.com>
2017-01-05 19:04:24 -0600

    added port "functionabliity"

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'pug');
 app.use(express.static(path.join('public')));
 
@@ -9,7 +10,6 @@ app.get('/', function(req, res) {
   res.render('index');
 })
 
-app.listen(3000, function() {
-  console.log('Example app listen on port 3000!');
+app.listen(app.get('port'), function() {
+  console.log(`Example app listen on port ${app.get('port')}!`);
 })
-
