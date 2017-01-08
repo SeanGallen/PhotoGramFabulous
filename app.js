b52b43c0ffae6e82b7a285f5b0e4a4cd992d8602
Sean <sjgallen@gmail.com>
2017-01-07 18:21:39 -0600

    added routes and fixed css positioning

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ app.set('port', process.env.PORT || 8080);
 app.set('view engine', 'pug');
 app.use(express.static(path.join('public')));
 
-app.get('/', function(req, res) {
-  res.render('index');
-})
+app.use(require('./routes/index'));
 
 app.listen(app.get('port'), function() {
   console.log(`Example app listen on port ${app.get('port')}!`);
